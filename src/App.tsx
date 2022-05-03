ecee2dfd12d0d5cb66037a682f62c3071c929dee
Victor Santelé <victor.santele@gmail.com>
2022-05-03 22:31:03 +0200

    add event listener

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
-import { Map as LeafletMap } from "leaflet"
+import { Map as LeafletMap, Polyline } from "leaflet"
 import { useEffect, useState } from "react"
 
 import Map from "./components/Map"
+
+function randomColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`
+}
 
 function App() {
   const [map, setMap] = useState<LeafletMap | null>(null)
@@ -22,6 +26,47 @@ function App() {
         editMode: true,
         removalMode: true,
       })
+
+      // disable snappable
+      // map.pm.setGlobalOptions({
+      //   ...map.pm.getGlobalOptions(),
+      //   snappable: false,
+      // })
+
+      map.on("pm:drawstart", ({ shape, workingLayer }) => {
+        console.log("drawstart", shape)
+        workingLayer.on("pm:vertexadded", ({ workingLayer, latlng }) => {
+          const workingShape = workingLayer as Polyline
+          workingShape.setStyle({ color: randomColor() })
+          console.log("Add Vertex: ", latlng)
+        })
+      })
+
+      map.on("pm:drawend", () => {
+        console.log("drawend")
+      })
+
+      map.on("pm:create", ({ shape, layer }) => {
+        if (shape === "Line") {
+          const line = layer as Polyline
+          console.dir(line.toGeoJSON())
+        }
+        layer.on("pm:update", () => {
+          console.log("update")
+        })
+        layer.on("pm:remove", () => {
+          console.log("remove")
+        })
+        layer.on("pm:dragend", () => {
+          console.log("dragend")
+        })
+      })
+
+      return () => {
+        map.off("pm:drawstart")
+        map.off("pm:drawend")
+        map.off("pm:create")
+      }
     }
   }, [map])
 
